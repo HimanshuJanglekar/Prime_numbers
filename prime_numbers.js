5da09c172bd95d6a2b79ee735e30bed7df00d42e
sumit_ss <sumitshashtrakar@gmail.com>
2022-10-20 22:45:02 +0530

    corect code

--- prime_numbers.js
+++ prime_numbers.js
@@ -1,7 +1,18 @@
 // check number prime or not
 
 let num=11;
-
+let count=0
 for(let i=1; i <= num; i++){
-if(num%2==0
+
+        if(num%i==0){
+            count++
+        }
+}
+
+if(count==2){
+    console.log('num is a Prime Number')
+}
+else{
+    console.log('num is not a Prime Number')
+
 }
